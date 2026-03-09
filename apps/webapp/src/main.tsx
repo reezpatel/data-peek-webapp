6b305d5582d218fd3b2cbe9fd613ee24d8c370b5
Reez <reezpatel@gmail.com>
2026-03-09 09:12:53 +0530

    fix: crypto.randomUUID in webapp

--- apps/webapp/src/main.tsx
+++ apps/webapp/src/main.tsx
@@ -1,5 +1,14 @@
 import './assets/global.css'
 
+// Polyfill crypto.randomUUID for non-secure contexts (HTTP)
+if (typeof crypto !== 'undefined' && typeof crypto.randomUUID === 'undefined') {
+  ;(crypto as any).randomUUID = () => {
+    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: any) =>
+      (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
+    )
+  }
+}
+
 import { SocketTransport } from './socket'
 import { createApi } from './api-shim'
 
